1aa5b2bdb1fb132037cd047a2b73c2cd601950a9
kkh1396 <100066522+kkh1396@users.noreply.github.com>
2025-02-04 19:21:34 +0900

    2025-02-04
    학습

--- 02_java/05_Array/src/Practice/CompExample.java
+++ 02_java/05_Array/src/Practice/CompExample.java
@@ -1,6 +1,9 @@
 package Practice;
 
+import java.util.Arrays;
 import java.util.Scanner;
+
+import javax.swing.SortOrder;
 
 public class CompExample {
 
@@ -18,34 +21,151 @@ public class CompExample {
 //		method10();
 //		method11();
 //		method12();
-		method13();
+//		method13();
+//		method14();
+//		method15();
+		method16();
 	}
 	
-	public static void method13() {
+	public static void method16() {
 		
-		int[] Rarr = new int[10];
-		int i = 0;
-		while (i<Rarr.length) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("배열의 크기를 입력하세요 : ");
+		int num = sc.nextInt();
+		
+		String[] str = new String[num];
+		int i;
+		for(i=0; i<num;i++) {
+			System.out.printf("%d번쨰 문자열 : ",i);
+			String st1 = sc.nextLine();
+			str[i] = st1;
+		}
+		
+		while (true) {
 			
-			int ran = (int)(Math.random()*10 +1);
-			Rarr[i] = ran;
+			System.out.print("더 값을 입력하시겠습니까?(Y/N) : ");
+			int num1 = sc.nextInt();
 			
-			for(int j=0; i<Rarr.length; j++) {
-				
-			}
+		    int	num2 = num + num1;
+		    
+			String[] str1 = new String[num1];
 			
-			i++;
+		
+			
+			
+			
+			
+			
+			
+			
+			System.out.print("1번째 문자열 : ");
+			//String s
+			
 			
 			
 			
 		}
 		
 		
-//		for (int i=0; i<Rarr.length;i++) {
-//			int ran = (int)(Math.random()*10 +1);
-//			Rarr[i] = ran;
-//			System.out.print(Rarr[i]+ " ");
+	}
+	
+	public static void method15() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("문자열 : ");
+		String str = sc.nextLine();
+		
+		char[] chArr = new char[str.length()];
+		int idx =0;
+		for( int i=0; i<str.length(); i++) {
+//			chArr[i] = str.charAt(i);
+		
+			//	중복된 문자 제거
+			
+			boolean isDup = false;	
+			for (int j =0; j< i; j++ ) {
+				if (str.charAt(i) ==  chArr[j]) {
+					isDup = true;
+					break;
+				}
+				}
+		 	
+//			if (isDup == false) 
+			if (!isDup){
+				chArr[idx++] = str.charAt(i);
+			}
+		}
+		
+		System.out.print("문자열에 있는 문자 : ") ;
+		for (int i=0; i<idx; i++) {
+			System.out.print(chArr[i]);
+			
+			if (i <idx-1) {
+				System.out.print(", ");
+			}
+		}
+		System.out.println("") ;
+		System.out.print("문자 개수 : " + idx) ;
+		
+//		System.out.print("문자열에 있는 문자 : ") ;
+//		for (int i=0; i<chArr.length; i++) {
+//			System.out.print(chArr[i]+ " ") ;
 //		}
+//		System.out.
+		
+		
+	}
+	
+	
+	public static void method14() {
+		int[] Rarr = new int[6];
+
+		// 랜덤값 대입
+		
+		for (int i=0; i<Rarr.length; i++) {
+			Rarr[i]=(int)(Math.random()*45 +1);
+						
+			
+			for (int j=0; j<i; j++ ) {
+				if (Rarr[i] == Rarr[j] ) {
+					i--;
+					break;
+				}
+			}
+		}
+		
+		Arrays.sort(Rarr);
+		for (int i=0; i<Rarr.length;i++) {
+			System.out.print(Rarr[i] + " ");
+		}
+	}
+	
+	public static void method13() {
+		
+		int[] Rarr = new int[10];
+
+		// 랜덤값 대입
+		
+		for (int i=0; i<Rarr.length; i++) {
+			Rarr[i]=(int)(Math.random()*10 +1);
+			System.out.println("첫for문 : " +   i  + " "  + Rarr[i]);
+			
+			
+			for (int j=0; j<i; j++ ) {
+				if (Rarr[i] == Rarr[j] ) {
+					
+					System.out.println("두번쨰for문 i : " + i+ " " + Rarr[i]);
+					System.out.println("두번쨰for문 j : " + j+ " " + Rarr[j]);
+					i--;
+					break;
+				}
+			}
+		}
+		
+		for (int i=0; i<Rarr.length;i++) {
+			System.out.print(Rarr[i]+" ");
+		}
 
 		
 		
